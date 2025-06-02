97d546a69f1072ddff1d97933e2830743b569162
JulienToumieh <130232510+JulienToumieh1@users.noreply.github.com>
2025-06-02 17:36:36 +0300

    Fixed all book images

--- src/app/book-item/book-item.component.ts
+++ src/app/book-item/book-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-book-item',
@@ -8,4 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 export class BookItemComponent {
   @Input() book: any; 
+  apiUrl: string = environment.apiUrl
 }
